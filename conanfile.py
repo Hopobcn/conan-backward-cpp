34c75f3d7e891ab021013f272f615c136294a655
Pau Farré <paurubifg@gmail.com>
2019-08-04 20:02:30 +0200

    Implement package fro v1.3

--- conanfile.py
+++ conanfile.py
@@ -1,71 +1,75 @@
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
+class BackwardCppConan(ConanFile):
+    name = "backward-cpp"
+    version = "1.3"
+    description = "A beautiful stack trace pretty printer for C++"
+    topics = ("conan", "backward-cpp", "stack-trace")
+    url = "https://github.com/hopobcn/conan-backward-cpp"
+    homepage = "https://github.com/bombela/backward-cpp"
+    author = "Hopobcn <hopobcn@gmail.com>"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = [ "CMakeLists.txt" ]
     generators = "cmake"
 
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+    options = {
+       "stack_walking_unwind": [True, False],
+       "stack_walking_backtrace": [True, False],
+       "stack_details_auto_detect": [True, False],
+       "stack_details_backtrace_symbol": [True, False],
+       "stack_details_dw": [True, False],
+       "stack_details_bfd": [True, False],
+       "shared": [True, False]
+    }
+    default_options = {
+       "stack_walking_unwind": True,
+       "stack_walking_backtrace": False,
+       "stack_details_auto_detect": True,
+       "stack_details_backtrace_symbol": False,
+       "stack_details_dw": False,
+       "stack_details_bfd": False,
+       "shared": False
+    }
 
-    # Custom attributes for Bincrafters recipe conventions
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = (
-        "OpenSSL/1.0.2s@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    def configure(self):
+        if not self.settings.compiler.cppstd:
+            self.settings.compiler.cppstd = 11
+        elif self.settings.compiler.cppstd.value < "11":
+                raise ConanInvalidConfiguration("backward-cpp requires c++11")
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
+        sha256 = "4bf3fb7029ff551acda6578d9d8e13d438ebdd82a787a82b157728e3af6b5dec"
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version, sha256=sha256))
         extracted_dir = self.name + "-" + self.version
-
-        # Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder, defs={'BACKWARD_' + name.upper(): value for name, value in self.options.values.as_list()})
         return cmake
 
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
+        cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,58 @@
+#include <backward.hpp>
 #include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+using namespace backward;
+
+class TracedException : public std::runtime_error
+{
+public:
+    TracedException() :
+        std::runtime_error(_get_trace())
+    {}
+
+private:
+    std::string _get_trace()
+    {
+        std::ostringstream ss;
+
+        StackTrace stackTrace;
+        TraceResolver resolver;
+        stackTrace.load_here();
+        resolver.load_stacktrace(stackTrace);
+
+        for(std::size_t i = 0; i < stackTrace.size(); ++i)
+        {
+            const ResolvedTrace trace = resolver.resolve(stackTrace[i]);
+
+            ss << "#" << i << " at " << trace.object_function << "\n";
+        }
+
+        return ss.str();
+    }
+};
+
+void f(int i)
+{
+    if(i >= 42)
+    {
+        throw TracedException();
+    }
+    else
+    {
+        std::cout << "i=" << i << "\n";
+        f(i + 1);
+    }
+}
 
 int main()
 {
-    std::cout << "Bincrafters\n";
-    return 0;
+    try
+    {
+        f(0);
+    } catch (const TracedException& ex)
+    {
+        std::cout << ex.what();
+    }
 }
