a0760c3491d8c6ffc9535f8e036ee02c4309b2f2
pfarre <paurubifg@gmail.com>
2019-08-04 23:36:17 +0200

    Define dependencies programatically
    Linux uses unwind provided by gcc/clang + libdwarf(conan)
    Macos uses unwind provided by gcc/clang + backtrace

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+#from conans.error import ConanInvalidConfiguration
 import os
 
 
@@ -14,7 +15,7 @@ class BackwardCppConan(ConanFile):
     author = "Hopobcn <hopobcn@gmail.com>"
     license = "MIT"
     exports = ["LICENSE.md"]
-    exports_sources = [ "CMakeLists.txt" ]
+    exports_sources = [ "CMakeLists.txt", "0001-install.patch" ]
     generators = "cmake"
 
     settings = "os", "arch", "compiler", "build_type"
@@ -25,7 +26,7 @@ class BackwardCppConan(ConanFile):
     options = {
        "stack_walking_unwind": [True, False],
        "stack_walking_backtrace": [True, False],
-       "stack_details_auto_detect": [True, False],
+       "stack_details_auto_detect": [False], # dont let backtrace auto decide
        "stack_details_backtrace_symbol": [True, False],
        "stack_details_dw": [True, False],
        "stack_details_bfd": [True, False],
@@ -34,10 +35,10 @@ class BackwardCppConan(ConanFile):
     default_options = {
        "stack_walking_unwind": True,
        "stack_walking_backtrace": False,
-       "stack_details_auto_detect": True,
+       "stack_details_auto_detect": False,
        "stack_details_backtrace_symbol": False,
        "stack_details_dw": False,
-       "stack_details_bfd": False,
+     # "stack_details_bfd": False,
        "shared": False
     }
 
@@ -45,16 +46,31 @@ class BackwardCppConan(ConanFile):
     _build_subfolder = "build_subfolder"
 
     def configure(self):
+        if self.settings.os not in ["Linux", "Macos", "Android"]:
+            raise ConanInvalidConfiguration("backward-cpp is not supported by your platform.")
         if not self.settings.compiler.cppstd:
             self.settings.compiler.cppstd = 11
-        elif self.settings.compiler.cppstd.value < "11":
-                raise ConanInvalidConfiguration("backward-cpp requires c++11")
-
+        
+    # Warning: Setting options conditionally is FINAL and values will not be overridable
+    #          from downstream dependent packages
+    def config_options(self):
+        if self.settings.os in ["Linux", "Android"]:
+            # on Linux we want to define BACKWARD_HAS_UNWIND + BACKWARD_HAS_DWARF
+            self.options.stack_details_bfd = False
+        elif self.settings.os == "Macos":
+            # on Macos we want to define BACKWARD_HAS_UNWIND + BACKWARD_HAS_BACKTRACE_SYMBOL
+            self.options.stack_details_bfd = True
+        
+    def requirements(self):
+        if self.settings.os == "Linux":
+            self.requires("libdwarf/20190505@bincrafters/stable")
+        
     def source(self):
         sha256 = "4bf3fb7029ff551acda6578d9d8e13d438ebdd82a787a82b157728e3af6b5dec"
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version, sha256=sha256))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+        tools.patch(base_path=self._source_subfolder, patch_file="0001-install.patch")
 
     def _configure_cmake(self):
         cmake = CMake(self)
